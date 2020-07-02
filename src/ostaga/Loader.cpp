d6a24afa2ee3eba133b7fccf86ee274afa165382
Sympleque <xamser7@gmail.com>
2020-07-02 01:38:31 -0500

    Enabled hybrid optimus for high performance

--- src/ostaga/Loader.cpp
+++ src/ostaga/Loader.cpp
@@ -3,6 +3,18 @@
 #include "ospch.h"
 //
 
+#if defined(OSTAGA_BUILD_WINDOWS) && defined(OSTAGA_DIST)
+
+	// Enable optimus for hybrid graphics systems in order to 
+	// use high performance graphics.
+
+	extern "C" {
+		__declspec(dllexport) unsigned long NvOptimusEnablement = 0x01;
+		__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 0x01;
+	}
+
+#endif
+
 #include <Ostaga.h>
 #include <window/Window.h>
 #include <Application.h>
