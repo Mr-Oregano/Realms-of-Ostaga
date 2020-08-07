a812371917b003cc67754d644d4275c8de5922fc
Sympleque <xamser7@gmail.com>
2020-08-06 23:23:55 -0500

    Using git LFS for large binary assets

--- res/shaders/renderer-shader-frag.glsl
+++ res/shaders/renderer-shader-frag.glsl
@@ -1,24 +1,3 @@
-#version 460 core
-
-layout(location = 0) out vec4 o_Color;
-
-uniform sampler2D u_Atlas;
-
-in vec4 vf_Tint;
-in vec2 vf_TexCoords;
-in vec2 vf_TexelSize;
-
-void main()
-{
-	vec2 Texel_TexCoords = vf_TexelSize * vf_TexCoords;
-
-	vec2 alpha = 0.7 * vec2(dFdx(Texel_TexCoords.x), dFdy(Texel_TexCoords.y));
-	vec2 CurrentFragInTexel = fract(Texel_TexCoords);
-	vec2 CurrentTexel = floor(Texel_TexCoords);
-	vec2 FilterOffset = clamp(CurrentFragInTexel / (2.0 * alpha), 0.0, 0.5)
-					  + clamp((CurrentFragInTexel - 1.0) / (2.0 * alpha) + 0.5, 0.0, 0.5);
-
-	vec2 FilteredCoords = (CurrentTexel + FilterOffset) / vf_TexelSize;
-
-	o_Color = texture(u_Atlas, FilteredCoords) * vf_Tint;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a9e22aaf7b7784a066629fb02428cb0e8e33c4e7a94c3ac2a6d52734a1c61497
+size 690
--- res/shaders/renderer-shader-vert.glsl
+++ res/shaders/renderer-shader-vert.glsl
@@ -1,26 +1,3 @@
-#version 460 core
-
-layout(location = 0) in vec4 a_Dimensions;
-layout(location = 1) in float a_Rotation;
-layout(location = 2) in vec4 a_Tint;
-layout(location = 3) in vec2 a_TexelSize;
-layout(location = 4) in vec4 a_TexCoords;
-
-out vec2 v_Pos;
-out vec2 v_Size;
-out float v_Rot;
-
-out vec4 v_Tint;
-out vec2 v_TexelSize;
-out vec4 v_TexCoords;
-
-void main()
-{
-	v_Pos = a_Dimensions.xy;
-	v_Size = a_Dimensions.zw;
-	v_Rot = a_Rotation;
-
-	v_Tint = a_Tint;
-	v_TexelSize = a_TexelSize;
-	v_TexCoords = a_TexCoords;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:bc1ac06b65927e448e6d40edd9edab97a9de0d340a043461c91d7ed910d532ee
+size 528
